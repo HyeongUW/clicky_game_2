4e5c99fb59ee24f5cd8467492b1b769d6ab6d12b
Hyeong Suk Kim <whddkf2004@gmail.com>
2019-11-11 03:27:12 -0800

    Updates

---
+++ precache-manifest.9a5602d598ae0a9473d3916edc725aed.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18460f9f67faa42e53062771eb0714cc",
+    "url": "/clicky_game_2/index.html"
+  },
+  {
+    "revision": "b3c9a1ddcfe8b4085cd5",
+    "url": "/clicky_game_2/static/css/main.400d4529.chunk.css"
+  },
+  {
+    "revision": "013dd4c7346b41c72fcc",
+    "url": "/clicky_game_2/static/js/2.8d779288.chunk.js"
+  },
+  {
+    "revision": "b3c9a1ddcfe8b4085cd5",
+    "url": "/clicky_game_2/static/js/main.427d2ea0.chunk.js"
+  },
+  {
+    "revision": "82e75b1f5d50797342a0",
+    "url": "/clicky_game_2/static/js/runtime-main.75812be1.js"
+  },
+  {
+    "revision": "cbc1c870c9c053fce92f21caa2a6b5c6",
+    "url": "/clicky_game_2/static/media/1.balloon.cbc1c870.png"
+  },
+  {
+    "revision": "017f50a75a92a54bbc0ba085f1fbe49b",
+    "url": "/clicky_game_2/static/media/10.017f50a7.png"
+  },
+  {
+    "revision": "32572bb0e4b14c04402312a4e470f0a8",
+    "url": "/clicky_game_2/static/media/11.32572bb0.png"
+  },
+  {
+    "revision": "796194274d86641daa471e68b7222744",
+    "url": "/clicky_game_2/static/media/12.79619427.png"
+  },
+  {
+    "revision": "8242b5f5b21f067095cc8d90593d6af4",
+    "url": "/clicky_game_2/static/media/2.raider.8242b5f5.png"
+  },
+  {
+    "revision": "ab1264f978c715a6ce892132b8ce21d0",
+    "url": "/clicky_game_2/static/media/3.hitting a pip-boy.ab1264f9.png"
+  },
+  {
+    "revision": "d0c902fdc989da9c40890c4ccfe7ea92",
+    "url": "/clicky_game_2/static/media/4.prisoner.d0c902fd.png"
+  },
+  {
+    "revision": "2899a5295f5ad4fe8f93ff3202d02736",
+    "url": "/clicky_game_2/static/media/5.chicago type writer.2899a529.png"
+  },
+  {
+    "revision": "0cf8b465afd1e5831c030fb996ae52bd",
+    "url": "/clicky_game_2/static/media/6.humming.0cf8b465.png"
+  },
+  {
+    "revision": "ca1b798c033fc7ae6c0705a3ecea6aea",
+    "url": "/clicky_game_2/static/media/7.heavyload.ca1b798c.png"
+  },
+  {
+    "revision": "cf769ff9a8440b21be22a4d7993f343c",
+    "url": "/clicky_game_2/static/media/8.like.cf769ff9.png"
+  },
+  {
+    "revision": "f7f1d912b78104d32437646c0fb1a71f",
+    "url": "/clicky_game_2/static/media/9.greedy.f7f1d912.png"
+  },
+  {
+    "revision": "1899e8998b753e8fb1d880e837fcea0f",
+    "url": "/clicky_game_2/static/media/Overseer.1899e899.otf"
+  }
+]);
