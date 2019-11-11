282cce211c4b222af68041d516f18916cf881284
Hyeong Suk Kim <whddkf2004@gmail.com>
2019-11-11 02:56:39 -0800

    Updating

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import balloon from './images/1.balloon.png';
+import raider from './images/2.raider.png';
+import pipBoy from './images/3.hitting a pip-boy.png';
+import prisoner from './images/4.prisoner.png';
+import chicago from './images/5.chicago type writer.png';
+import humming from './images/6.humming.png';
+import heavy from './images/7.heavyload.png';
+import like from './images/8.like.png';
+import greedy from './images/9.greedy.png';
+import celeb from './images/10.png';
+import mind from './images/11.png';
+import spy from './images/12.png';
+
+const images = [
+  balloon,
+  raider,
+  pipBoy,
+  prisoner,
+  chicago,
+  humming,
+  heavy,
+  like,
+  greedy,
+  celeb,
+  mind,
+  spy
+];
+
+export default images;
